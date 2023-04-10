59e31f92af425fe8a7e492c56262d07b26d2e39b
usamaislam013 <usama0islam@gmail.com>
2023-04-10 20:44:50 +0500

    Resolve pipeline, Add lineitem

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :quotes
 
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
+  
   root to: "pages#home"
 end
